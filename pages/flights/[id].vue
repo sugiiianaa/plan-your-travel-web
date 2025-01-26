3d2330b32948f02ec9629e32c155ffb4bbb98622
sugiiianaa <sugiiianaa@gmail.com>
2025-01-26 09:07:22 +0700

    Intial setup

---
+++ pages/flights/[id].vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Hello from flight number {{ $route.params.id }} !!</h1>
+  </div>
+</template>
---
+++ pages/flights/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Hello from flight!!</h1>
+  </div>
+</template>
